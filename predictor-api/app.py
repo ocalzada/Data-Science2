85cdd8fe6b52bc89322e08d9f30b46bc9745605b
ToddMG <munyontodd@gmail.com>
2019-11-20 15:39:19 -0800

    Updated requirements.txt

--- predictor-api/app.py
+++ predictor-api/app.py
@@ -3,7 +3,6 @@ from flask import Flask, request
 from flask import jsonify
 from .resnet50 import process_img_path, resnet_model
 
-
 def create_app():
     app = Flask(__name__)
 
@@ -12,10 +11,10 @@ def create_app():
         '''a route that expects an image url and id. returns image classifications, probabilities, and id'''
         # get info from backend 
         lines = request.get_json(force=True)
-
-        # # get strings from json
+    
+        # get strings from json
         url = lines['url']
-        photo_id = lines['photo_id']
+        photo_id = lines['photo_id'] 
 
         # make sure the input's correct
         assert isinstance(url, str)
@@ -24,6 +23,7 @@ def create_app():
         # process image and predict
         predictions = resnet_model(process_img_path(url))
 
+        # Return JSON object with photo_id and a list of predictions as a string
         return jsonify(photo_id=photo_id,
                        predictions=str(predictions))
 
